490bee421d4071a0808a4037a6a106935bcfbc29
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-21 01:20:42 +0200

    Add files via upload

---
+++ js-switch-exo-1/main.js
@@ -0,0 +1,24 @@
+let jour = prompt("Quelle jour de la semaine").trim().toLocaleLowerCase();
+switch (jour) {
+    case 'lundi':
+        alert("Quidditch");
+        break;
+    case 'mardi':
+        alert("Tournoi de pétanque");
+        break;
+    case 'mercredi':
+        alert("foot");
+        break;
+    case 'jeudi':
+        alert("Repos");
+        break;
+    case 'vendredi':
+        alert("MolenGeek");
+        break;
+    case 'samedi':
+        alert("Quidditch");
+        break;
+    case 'dimanche':
+        alert("Tournoi de pétanque");
+        break;
+}
